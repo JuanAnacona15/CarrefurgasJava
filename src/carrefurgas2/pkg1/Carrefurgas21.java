a5d0df2075421266ac70777a21d25f23c8ce9a8f
JuanAnacona15 <juanmanuelanaconamorales@gmail.com>
2024-08-02 06:51:50 -0500

    Menu interface

---
+++ src/carrefurgas2/pkg1/Carrefurgas21.java
@@ -0,0 +1,12 @@
+package carrefurgas2.pkg1;
+
+public class Carrefurgas21 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
